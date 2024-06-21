cad84474ae7ac12e1a3ad4ba013b0690a9d1603c
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-06-21 10:54:37 -0300

    feat: configure project and create car entity

--- livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/CarDatabaseApplication.java
+++ livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/CarDatabaseApplication.java
@@ -1,13 +1,20 @@
 package com.packt.carDatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(
+		CarDatabaseApplication.class
+	);
 
 	public static void main(String[] args) {
+		// test dev tools
 		SpringApplication.run(CarDatabaseApplication.class, args);
+		logger.info("Application started");
 	}
 
 }
