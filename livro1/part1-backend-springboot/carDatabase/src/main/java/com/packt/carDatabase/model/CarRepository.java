c92626255ee14852e25ed637a32f8107a4f98c60
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-06-24 09:10:17 -0300

    create springdoc

--- livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/model/CarRepository.java
+++ livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/model/CarRepository.java
@@ -1,9 +1,12 @@
 package com.packt.carDatabase.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+
 
 @RepositoryRestResource(path="vehicles")
 public interface CarRepository extends CrudRepository<Car,Long>{
-    
+    List<Car> findByBrand(@Param("brand") String brand);
 }
