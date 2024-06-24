2c193c097da8057cccf1d25809fc8503aa724196
Marcos Valdecy Macedo Costa Leite <mvaldecy11@gmail.com>
2024-06-24 16:24:10 -0300

    complete user service configuration

--- livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/CarDatabaseApplication.java
+++ livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/CarDatabaseApplication.java
@@ -2,20 +2,34 @@ package com.packt.carDatabase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.packt.carDatabase.model.User;
+import com.packt.carDatabase.model.UserRepository;
+
 @SpringBootApplication
-public class CarDatabaseApplication {
+public class CarDatabaseApplication implements CommandLineRunner {
+	private final UserRepository userRepository;
+
+	public CarDatabaseApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	private static final Logger logger = LoggerFactory.getLogger(
 		CarDatabaseApplication.class
 	);
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("admin", "$2y$10$G2RhJHgK9MCROgbpx.VIoODYUkvICjRNRyYXB/fXYdMurIlTfzFI.", "ADMIN"));
+	}
 
 	public static void main(String[] args) {
 		// test dev tools
 		SpringApplication.run(CarDatabaseApplication.class, args);
 		logger.info("Application started");
+		
 	}
-
 }
--- livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/service/UserDetailsServiceImpl.java
+++ livro1/part1-backend-springboot/carDatabase/src/main/java/com/packt/carDatabase/service/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.packt.carDatabase.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
@@ -12,19 +13,19 @@ import com.packt.carDatabase.model.User;
 import com.packt.carDatabase.model.UserRepository;
 
 @Service
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl  implements UserDetailsService{
     private final UserRepository userRepository;
 
     public UserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<User> user = userRepository.findByUsername(username);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
         UserBuilder builder = null;
         if(user.isPresent()) {
             User currentUser = user.get();
-            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder = org.springframework.security.core.userdetails.User.withUsername(userName);
             builder.password(currentUser.getPassword());
             builder.roles(currentUser.getRole());
         } else {
